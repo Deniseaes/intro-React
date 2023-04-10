5c19368bc9655d2dc9694e30a27cdb43248242a6
Deniseaes <deniseaeschbacher99@gmail.com>
2023-04-10 18:02:12 -0300

    Contador App: Estoy creando un nuevo componente que al hacer click sobre el imprime en consola los valores 123

--- counter-app/src/main.jsx
+++ counter-app/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import Saludo from "./Saludo";
+import ContadorApp from "./ContadorApp";
 
 const name = "Denise Aeschbacher =)";
+const num = 123;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Saludo nombre={name} />
+    <ContadorApp value={num} />
   </React.StrictMode>
 );
